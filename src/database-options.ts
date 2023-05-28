839fbb9c0d45f364278830d0bd001c5295f7ff48
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2023-05-28 20:00:17 +0300

    feat: add dialectOptions

--- src/database-options.ts
+++ src/database-options.ts
@@ -4,25 +4,23 @@ import { User } from './auth/user.entity';
 const databaseOptions = () => {
   return {
     type: 'postgres',
-    // host: process.env.POSTGRES_HOST,
-    // port: +process.env.POSTGRES_PORT,
-    // username: process.env.POSTGRES_USERNAME,
-    // password: process.env.POSTGRES_PASSWORD,
-    // database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    port: +process.env.POSTGRES_PORT,
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
     synchronize: true,
     logging: true,
     entities: [User],
     subscribers: [],
     migrations: [],
     autoLoadEntities: true,
-    // dialectOptions: {
-    //   ssl: {
-    //     // require: true,
-    //     rejectUnauthorized: false,
-    //   },
-    // },
-    ssl: true,
-    url: 'postgres://wbyvskhyxyovmn:60b6bce9dad7d9d8e35f4c93883adaa519bdb4534eaa5ab2eaa70aac59c405c5@ec2-52-215-209-64.eu-west-1.compute.amazonaws.com:5432/d25bgs943p0qea',
+    dialectOptions: {
+      ssl: {
+        // require: true,
+        rejectUnauthorized: false,
+      },
+    },
   } as TypeOrmModuleOptions;
 };
 
